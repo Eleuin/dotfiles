10238b9fd5119f9fd25aa71463ba82cf34170b0f
Eleuin <nerdtaco@protonmail.ch>
2017-04-16 23:23:06 -0400

    Cleaned paths and added stuff to installer

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
+echo "installing xinitrc"
+cp .xinitrc ~/
+
+echo "installing Xresources"
+cp .Xresources ~/
+
 echo "installing zsh configs"
-cp .zshrc ~/.zshrc
-cp .zshalias ~/.zshalias
+cp .zshrc ~/
+cp .zshalias ~/
 
 echo "installing vim configs"
-cp .vimrc ~/.vimrc
+cp .vimrc ~/
 
 echo "installing i3 configs"
 cp .config/i3/config ~/.config/i3
+
+echo "installing termite configs"
+cp .config/termite/config ~/.config/termite
 
 echo "installing wallpaper"
 cp wallpaper.jpg ~/Pictures
