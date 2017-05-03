76565d2331c01ef760149431e42767a060580cff
Eleuin <nerdtaco@protonmail.ch>
2017-05-03 01:03:35 -0400

    Added config for cava

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
+#fixme for multiple platforms
 echo "installing software"
 pacaur -S xorg-xinit i3-gaps i3blocks  rofi-dmenu ponymix maim cmus
+
 
 echo "installing xinitrc"
 cp .xinitrc ~/
@@ -19,6 +23,9 @@ cp .config/i3/config ~/.config/i3
 
 echo "installing termite configs"
 cp .config/termite/config ~/.config/termite
+
+echo "installing cava configs"
+cp .config/cava/config ~/.config/cava
 
 echo "installing wallpaper"
 cp wallpaper.jpg ~/Pictures
